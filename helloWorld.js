daf3d87f9adb6315143594934de7081a9f7c3b0f
Edgar Figueroa <edgarefigueroa@outlook.com>
2022-06-22 14:02:05 -0400

    intro

---
+++ helloWorld.js
@@ -0,0 +1,44 @@
+console.log("Hello, Universe")
+
+const firstName = "Edgar";
+let age = 99;
+let height;
+const isCool = true;
+
+// console.log(typeof firstName)
+// console.log(typeof age)
+// console.log(typeof isCool)
+// onsole.log(typeof height)
+
+let x = 4;
+let y = 10;
+let z = 7+9;
+let sum = x + y+ z;
+console.log(sum);
+
+let diff = (x+y) - (y+z)
+console.log(sum)
+
+let string = 'Hello, Universe'
+
+let welcomeString = `Hello, University ${firstName}`
+
+console.log(welcomeString)
+
+if(age>=21){
+    console.log(`you can drink ${firstName}`)
+}
+
+let score = 78
+passing =40
+
+if(score>48){
+    console.log("you pass")
+}
+else{
+    "Try again"
+}
+
+if(score !==passing){
+    console.log()
+}
